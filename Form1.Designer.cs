6408b445b0414eea8068b4c8c33f63a33b07ac59
Gilmar Freitas <57302448+gilmarfreitaspt@users.noreply.github.com>
2020-01-13 22:29:46 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Exercicio_4_Jogo_do_Galo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Jogador1 = new System.Windows.Forms.Label();
+            this.Jogador2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btt1 = new System.Windows.Forms.Button();
+            this.btt2 = new System.Windows.Forms.Button();
+            this.btt3 = new System.Windows.Forms.Button();
+            this.btt6 = new System.Windows.Forms.Button();
+            this.btt5 = new System.Windows.Forms.Button();
+            this.btt4 = new System.Windows.Forms.Button();
+            this.btt9 = new System.Windows.Forms.Button();
+            this.btt8 = new System.Windows.Forms.Button();
+            this.btt7 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.lbl_j1 = new System.Windows.Forms.Label();
+            this.lbl_j2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Jogador1
+            // 
+            this.Jogador1.AutoSize = true;
+            this.Jogador1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jogador1.Location = new System.Drawing.Point(15, 9);
+            this.Jogador1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Jogador1.Name = "Jogador1";
+            this.Jogador1.Size = new System.Drawing.Size(78, 16);
+            this.Jogador1.TabIndex = 0;
+            this.Jogador1.Text = "Jogador 1";
+            // 
+            // Jogador2
+            // 
+            this.Jogador2.AutoSize = true;
+            this.Jogador2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jogador2.Location = new System.Drawing.Point(164, 9);
+            this.Jogador2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Jogador2.Name = "Jogador2";
+            this.Jogador2.Size = new System.Drawing.Size(78, 16);
+            this.Jogador2.TabIndex = 1;
+            this.Jogador2.Text = "Jogador 2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Image = global::Exercicio_4_Jogo_do_Galo.Properties.Resources.x_6060;
+            this.label3.Location = new System.Drawing.Point(99, 1);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 50);
+            this.label3.TabIndex = 2;
+            // 
+            // btt1
+            // 
+            this.btt1.Location = new System.Drawing.Point(21, 144);
+            this.btt1.Margin = new System.Windows.Forms.Padding(2);
+            this.btt1.Name = "btt1";
+            this.btt1.Size = new System.Drawing.Size(51, 56);
+            this.btt1.TabIndex = 3;
+            this.btt1.UseVisualStyleBackColor = true;
+            this.btt1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btt2
+            // 
+            this.btt2.Location = new System.Drawing.Point(85, 144);
+            this.btt2.Margin = new System.Windows.Forms.Padding(2);
+            this.btt2.Name = "btt2";
+            this.btt2.Size = new System.Drawing.Size(51, 56);
+            this.btt2.TabIndex = 4;
+            this.btt2.UseVisualStyleBackColor = true;
+            this.btt2.Click += new System.EventHandler(this.btt2_Click);
+            // 
+            // btt3
+            // 
+            this.btt3.Location = new System.Drawing.Point(149, 144);
+            this.btt3.Margin = new System.Windows.Forms.Padding(2);
+            this.btt3.Name = "btt3";
+            this.btt3.Size = new System.Drawing.Size(51, 56);
+            this.btt3.TabIndex = 5;
+            this.btt3.UseVisualStyleBackColor = true;
+            this.btt3.Click += new System.EventHandler(this.btt3_Click);
+            // 
+            // btt6
+            // 
+            this.btt6.Location = new System.Drawing.Point(149, 211);
+            this.btt6.Margin = new System.Windows.Forms.Padding(2);
+            this.btt6.Name = "btt6";
+            this.btt6.Size = new System.Drawing.Size(51, 56);
+            this.btt6.TabIndex = 8;
+            this.btt6.UseVisualStyleBackColor = true;
+            this.btt6.Click += new System.EventHandler(this.btt6_Click);
+            // 
+            // btt5
+            // 
+            this.btt5.Location = new System.Drawing.Point(85, 211);
+            this.btt5.Margin = new System.Windows.Forms.Padding(2);
+            this.btt5.Name = "btt5";
+            this.btt5.Size = new System.Drawing.Size(51, 56);
+            this.btt5.TabIndex = 7;
+            this.btt5.UseVisualStyleBackColor = true;
+            this.btt5.Click += new System.EventHandler(this.btt5_Click);
+            // 
+            // btt4
+            // 
+            this.btt4.Location = new System.Drawing.Point(21, 211);
+            this.btt4.Margin = new System.Windows.Forms.Padding(2);
+            this.btt4.Name = "btt4";
+            this.btt4.Size = new System.Drawing.Size(51, 56);
+            this.btt4.TabIndex = 6;
+            this.btt4.UseVisualStyleBackColor = true;
+            this.btt4.Click += new System.EventHandler(this.btt4_Click);
+            // 
+            // btt9
+            // 
+            this.btt9.Location = new System.Drawing.Point(150, 276);
+            this.btt9.Margin = new System.Windows.Forms.Padding(2);
+            this.btt9.Name = "btt9";
+            this.btt9.Size = new System.Drawing.Size(51, 56);
+            this.btt9.TabIndex = 11;
+            this.btt9.UseVisualStyleBackColor = true;
+            this.btt9.Click += new System.EventHandler(this.btt9_Click);
+            // 
+            // btt8
+            // 
+            this.btt8.Location = new System.Drawing.Point(86, 275);
+            this.btt8.Margin = new System.Windows.Forms.Padding(2);
+            this.btt8.Name = "btt8";
+            this.btt8.Size = new System.Drawing.Size(51, 56);
+            this.btt8.TabIndex = 10;
+            this.btt8.UseVisualStyleBackColor = true;
+            this.btt8.Click += new System.EventHandler(this.btt8_Click);
+            // 
+            // btt7
+            // 
+            this.btt7.Location = new System.Drawing.Point(21, 275);
+            this.btt7.Margin = new System.Windows.Forms.Padding(2);
+            this.btt7.Name = "btt7";
+            this.btt7.Size = new System.Drawing.Size(51, 56);
+            this.btt7.TabIndex = 9;
+            this.btt7.UseVisualStyleBackColor = true;
+            this.btt7.Click += new System.EventHandler(this.btt7_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button10.Location = new System.Drawing.Point(34, 344);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(63, 37);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Novo jogo";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button11.Location = new System.Drawing.Point(116, 344);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(68, 37);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Reset";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // lbl_j1
+            // 
+            this.lbl_j1.AutoSize = true;
+            this.lbl_j1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_j1.Location = new System.Drawing.Point(36, 35);
+            this.lbl_j1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_j1.Name = "lbl_j1";
+            this.lbl_j1.Size = new System.Drawing.Size(16, 16);
+            this.lbl_j1.TabIndex = 14;
+            this.lbl_j1.Text = "0";
+            // 
+            // lbl_j2
+            // 
+            this.lbl_j2.AutoSize = true;
+            this.lbl_j2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_j2.Location = new System.Drawing.Point(185, 35);
+            this.lbl_j2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_j2.Name = "lbl_j2";
+            this.lbl_j2.Size = new System.Drawing.Size(16, 16);
+            this.lbl_j2.TabIndex = 15;
+            this.lbl_j2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = global::Exercicio_4_Jogo_do_Galo.Properties.Resources.galo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(439, 423);
+            this.Controls.Add(this.lbl_j2);
+            this.Controls.Add(this.lbl_j1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.btt9);
+            this.Controls.Add(this.btt8);
+            this.Controls.Add(this.btt7);
+            this.Controls.Add(this.btt6);
+            this.Controls.Add(this.btt5);
+            this.Controls.Add(this.btt4);
+            this.Controls.Add(this.btt3);
+            this.Controls.Add(this.btt2);
+            this.Controls.Add(this.btt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Jogador2);
+            this.Controls.Add(this.Jogador1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Jogo do Galo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Jogador1;
+        private System.Windows.Forms.Label Jogador2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btt1;
+        private System.Windows.Forms.Button btt2;
+        private System.Windows.Forms.Button btt3;
+        private System.Windows.Forms.Button btt6;
+        private System.Windows.Forms.Button btt5;
+        private System.Windows.Forms.Button btt4;
+        private System.Windows.Forms.Button btt9;
+        private System.Windows.Forms.Button btt8;
+        private System.Windows.Forms.Button btt7;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label lbl_j1;
+        private System.Windows.Forms.Label lbl_j2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_4_Jogo_do_Galo
+{
+    public partial class Form1 : Form
+    {
+        int jog = 1; //vez do jogador
+        int jog1 = 0; //pontuação jogador
+        int jog2 = 0; //pontuação jogador
+        int[,] j1 = new int[3, 3]; //matriz do jogador 1
+        int[,] j2 = new int[3, 3]; //matriz do jogador 2
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int soma()
+        {
+            int soma = 0;
+            for (int i=0; i<3; i++)
+                for (int j=0; j <3; j ++)
+                    soma=soma + j1[i, j] + j2[i, j];
+            return soma;
+        }
+
+        public void ganhou()
+        {
+            if (jog == 2)
+            {
+                //verificar a primeira linha
+                if ((j1[0, 0] + j1[0, 1] + j1[0, 2] == 3) ||
+                 // verifica a segunda linha
+                 (j1[1, 0] + j1[1, 1] + j1[1, 2] == 3) ||
+                 // verifica a terceira linha
+                 (j1[2, 0] + j1[2, 1] + j1[2, 2] == 3) ||
+                 // verifica a primeira coluna
+                 (j1[0, 0] + j1[1, 0] + j1[2, 0] == 3) ||
+                 // verifica a segunda coluna
+                 (j1[0, 1] + j1[1, 1] + j1[2, 1] == 3) ||
+                 // verifica a terceira coluna
+                 (j1[0, 2] + j1[1, 2] + j1[2, 2] == 3) ||
+                 // verifica a diagonal
+                 (j1[0, 0] + j1[1, 1] + j1[2, 2] == 3) ||
+                 // verifica a outra diagonal
+                 (j1[0, 2] + j1[1, 1] + j1[2, 0] == 3)
+                 )
+                {
+                    MessageBox.Show("vitoria", "Ganhou o Jogador 1");
+                    jog1++; //acrescenta um ponto de vitória
+                    lbl_j1.Text = jog1 + "";
+                }
+                else
+                    if (soma()==9)
+                {
+                    MessageBox.Show("Empate");
+                }
+            }
+            else
+
+            {
+                //verificar a primeira linha
+                if ((j1[0, 0] + j1[0, 1] + j1[0, 2] == 3) ||
+                 // verifica a segunda linha
+                 (j2[1, 0] + j2[1, 1] + j2[1, 2] == 3) ||
+                 // verifica a terceira linha
+                 (j2[2, 0] + j2[2, 1] + j2[2, 2] == 3) ||
+                 // verifica a primeira coluna
+                 (j2[0, 0] + j2[1, 0] + j2[2, 0] == 3) ||
+                 // verifica a segunda coluna
+                 (j2[0, 1] + j2[1, 1] + j2[2, 1] == 3) ||
+                 // verifica a terceira coluna
+                 (j2[0, 2] + j2[1, 2] + j2[2, 2] == 3) ||
+                 // verifica a diagonal
+                 (j2[0, 0] + j2[1, 1] + j2[2, 2] == 3) ||
+                 // verifica a outra diagonal
+                 (j2[0, 2] + j2[1, 1] + j2[2, 0] == 3)
+                 )
+                {
+                    MessageBox.Show("Vitoria", "Ganhou o Jogador 2");
+                    jog2++; //acrescenta um ponto de vitoria
+                    lbl_j2.Text = jog2 + "";
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            btt1.Image = null;
+            btt2.Image = null;
+            btt3.Image = null;
+            btt4.Image = null;
+            btt5.Image = null;
+            btt6.Image = null;
+            btt7.Image = null;
+            btt8.Image = null;
+            btt9.Image = null;
+
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    j1[i, j] = 0;
+                    j2[i, j] = 0;
+                }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt1.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[0, 0] = 1;
+            }
+            else
+            {
+                btt1.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[0, 0] = 1;
+            }
+
+            ganhou();
+        }
+
+   
+
+        private void btt2_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt2.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[0, 1] = 1;
+            }
+            else
+            {
+                btt2.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[0, 1] = 1;
+            }
+            ganhou();
+        }
+        private void btt3_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt3.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[0, 2] = 1;
+            }
+            else
+            {
+                btt3.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[0, 2] = 1;
+            }
+            ganhou();
+        }
+
+
+        private void btt4_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt4.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[1, 0] = 1;
+            }
+            else
+            {
+                btt4.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[1, 0] = 1;
+            }
+            ganhou();
+        }
+
+
+        private void btt5_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt5.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[1, 1] = 1;
+            }
+            else
+            {
+                btt5.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[1, 1] = 1;
+            }
+            ganhou();
+        }
+
+        private void btt6_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt6.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[1, 2] = 1;
+            }
+            else
+            {
+                btt6.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[1, 2] = 1;
+            }
+            ganhou();
+        }
+
+        private void btt7_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt7.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[2, 0] = 1;
+            }
+            else
+            {
+                btt7.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[2, 0] = 1;
+            }
+            ganhou();
+        }
+
+        private void btt8_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt8.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[2, 1] = 1;
+            }
+            else
+            {
+                btt8.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[2, 1] = 1;
+            }
+            ganhou();
+        }
+
+        private void btt9_Click(object sender, EventArgs e)
+        {
+            if (jog == 1)
+            {
+                btt9.Image = Properties.Resources.x_6060;
+                jog++;
+                label3.Image = Properties.Resources.o_6060;
+                j1[2, 2] = 1;
+            }
+            else
+            {
+                btt9.Image = Properties.Resources.o_6060;
+                jog--;
+                label3.Image = Properties.Resources.x_6060;
+                j2[2, 2] = 1;
+            }
+            ganhou();
+        }
+
+     
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            btt1.Image = null;
+            btt2.Image = null;
+            btt3.Image = null;
+            btt4.Image = null;
+            btt5.Image = null;
+            btt6.Image = null;
+            btt7.Image = null;
+            btt8.Image = null;
+            btt9.Image = null;
+
+            jog1 = 0;
+            jog2 = 0;
+            lbl_j1.Text = "0";
+            lbl_j2.Text = "0";
+
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    j1[i, j] = 0;
+                    j2[i, j] = 0;
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
